22db1da46c2ff1288d5fce2ebf273f172a626353
Randy Tsao <randytsao24@gmail.com>
2020-04-13 10:14:02 -0400

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-## Randy Tsao's GH Page
-
-Hi, check out my stuff hosted on GitHub Pages for more details.
-
-### Site Details
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ precache-manifest.49195c95528a0786fa4eb9246729af78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41cd851fde2acb49886ec1fe3582dcd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1234c005a7210ad2f263",
+    "url": "/static/css/main.bc4a37ea.chunk.css"
+  },
+  {
+    "revision": "693eb7a875c65a3f4a75",
+    "url": "/static/js/2.a39f141d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a39f141d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1234c005a7210ad2f263",
+    "url": "/static/js/main.9ed3fb67.chunk.js"
+  },
+  {
+    "revision": "8119f6b8fb6e6cb50470",
+    "url": "/static/js/runtime-main.0b84d149.js"
+  }
+]);
