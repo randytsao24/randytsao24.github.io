c6cc5202c570dde883d747a64aebaa1063933e55
Randy Tsao <randytsao24@gmail.com>
2020-04-14 17:46:52 -0400

    Fill out personal info cards

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -29,17 +29,32 @@ const Personal = () => {
       <Grid className={infoGridContainer} container spacing={3}>
         <Grid item xs={12}>
           <Paper className={infoCard} variant="outlined">
-            I'm a software engineer based in Brooklyn and I want to help make things with other people.
+            I'm a software engineer based in Brooklyn from Southern California and I want to help make things with other people.
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={infoCard} variant="outlined">
+            Reach out to me at <a href="mailto:randytsao24@gmail.com" target="_top">randytsao24@gmail.com</a>, check out my work info at my <a href="https://www.linkedin.com/in/randy-tsao/" target="_top">LinkedIn</a>, or see some really outdated repos at my <a href="https://github.com/randytsao24" target="_top">GitHub</a>.
           </Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
           <Paper className={infoCard} variant="outlined">
-            This is a placeholder for apps I'm gonna showcase.
+            I'm currently most proficient in full stack, JavaScript-based web app development.
           </Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
           <Paper className={infoCard} variant="outlined">
-            So is this!
+            Technologies relating to that include Node, React, Angular, MongoDB, Postgres, and AWS (Lambda/EB/Step Functions).
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={infoCard} variant="outlined">
+            Even though I am the kind of person that works to live, I am grateful for software engineering allowing me the opportunity to work with brilliant, talented people and I look forward to many more such experiences.
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper style={{ backgroundColor: '#9DD9F3' }} className={infoCard} variant="outlined">
+            Thanks for coming on here and take it easy. :)
           </Paper>
         </Grid>
       </Grid>
